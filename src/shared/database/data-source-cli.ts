4568926fcc2c42f2255f388052b2dba78349b13b
wellingtonsouza96 <wellington.santos@granatech.com.br>
2026-02-04 23:10:26 -0300

    feat: add new migrations cars

--- src/shared/database/data-source-cli.ts
+++ src/shared/database/data-source-cli.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata';
 import dotenv from 'dotenv';
-import { DataSource as TypeORMDataSource } from 'typeorm';
 import path from 'path';
+import { DataSource as TypeORMDataSource } from 'typeorm';
 
 dotenv.config();
 
--- src/shared/database/migrations/1770170753262-CreateCars.ts
+++ src/shared/database/migrations/1770170753262-CreateCars.ts
@@ -1,11 +1,76 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class CreateCars1770170753262 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    // Adicione suas alterações aqui
+    await queryRunner.createTable(
+      new Table({
+        name: 'cars',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true
+          },
+          {
+            name: 'name',
+            type: 'varchar'
+          },
+          {
+            name: 'description',
+            type: 'varchar'
+          },
+          {
+            name: 'daily_rate',
+            type: 'numeric'
+          },
+          {
+            name: 'available',
+            type: 'boolean',
+            default: true
+          },
+          {
+            name: 'license_plate',
+            type: 'varchar'
+          },
+          {
+            name: 'fine_amount',
+            type: 'numeric'
+          },
+          {
+            name: 'brand',
+            type: 'varchar'
+          },
+          {
+            name: 'category_id',
+            type: 'uuid',
+            isNullable: true
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()'
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()'
+          }
+        ],
+        foreignKeys: [
+          {
+            name: 'FKCategoryCar',
+            referencedTableName: 'categories',
+            referencedColumnNames: ['id'],
+            columnNames: ['category_id'],
+            onDelete: 'SET NULL',
+            onUpdate: 'SET NULL'
+          }
+        ]
+      })
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    // Adicione a reversão das alterações aqui
+    await queryRunner.dropTable('cars');
   }
 }
